36b02047ad0212457fbc98d54e01dfc4156cda63
Yacineos <96418831+Yacineos@users.noreply.github.com>
2023-10-21 22:48:21 +0200

    Delete ideas,Fgets usless files

--- ideas.c
+++
@@ -1,7 +0,0 @@
-// verif entrée string 
-/*
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-    fgets(data.val,sizeof(data.val),stdin);
-*/
-// prendre le nombre de processus comme argument de commande
